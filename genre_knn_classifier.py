f238e1486132bbb2d6ad2ed13c79b1caa0b58b6c
Justin Svegliato <justin.svegliato1@marist.edu>
2016-10-26 14:01:35 -0400

    [#2] Implemented a basic kNN

---
+++ genre_knn_classifier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import data_handler
+from sklearn.neighbors import KNeighborsClassifier
+
+CLASS_COUNT = 2
+TRAINING_EXAMPlE_COUNT = 200
+
+def main():
+    examples, labels = data_handler.get_data('example_data.csv', CLASS_COUNT)
+
+    training_examples, test_examples = data_handler.split_data(examples, TRAINING_EXAMPlE_COUNT)
+    training_labels, test_labels = data_handler.split_data(labels, TRAINING_EXAMPlE_COUNT)
+
+    classifier = KNeighborsClassifier(n_neighbors=5)
+    classifier.fit(training_examples, training_labels)
+
+    print 'Score %s' % classifier.score(test_examples, test_labels)
+
+if __name__ == '__main__':
+    main()
