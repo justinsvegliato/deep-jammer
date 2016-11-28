b94e35a3b44369aff6c278fddea0fe6b7343e0c1
Justin Svegliato <justin.svegliato1@marist.edu>
2016-11-28 13:42:56 -0500

    Created basic model architecture

--- generation/deep_jammer.py
+++ generation/deep_jammer.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from keras.optimizers import Nadam
+from keras.layers import LSTM, Dense, Activation, Reshape, Permute
 
-BATCH_SIZE = 10
-TIMESTEPS = 10
-INPUT_NOTE_COUNT = 80
+NUM_SEGMENTS = 10
+NUM_NOTES = 10
+NUM_TIMESTEPS = 10
+NUM_FEATURES = 10
+
+TIME_MODEL_LAYER_1 = 300
+TIME_MODEL_LAYER_2 = 300
+
+NOTE_MODEL_LAYER_1 = 100
+NOTE_MODEL_LAYER_2 = 50
+
+OUTPUT_LAYER = 2
 
 def main():
-    training_examples = []
-    training_labels = []
-
     model = Sequential([
-        LSTM(300, batch_input_shape=(BATCH_SIZE, TIMESTEPS, INPUT_NOTE_COUNT)),
-        LSTM(300),
-        LSTM(100),
-        LSTM(50)
+        # Lambda Add Dimension , input_shape=(NUM_SEGMENTS, NUM_TIMESTEPS, NUM_NOTES, NUM_FEATURES)
+        Reshape((NUM_SEGMENTS * NUM_NOTES, NUM_TIMESTEPS, NUM_FEATURES)),
+        # Lambda Remove Dimension
+        LSTM(TIME_MODEL_LAYER_1, return_sequences=True),
+        LSTM(TIME_MODEL_LAYER_2, return_sequences=True),
+        # Lambda Add Dimension
+        Reshape((NUM_SEGMENTS, NUM_NOTES, NUM_TIMESTEPS, NUM_FEATURES)),
+        Permute((1, 3, 2, 4)),
+        Reshape((NUM_SEGMENTS * NUM_TIMESTEPS, NUM_NOTES, NUM_FEATURES)),
+        # Lambda Remove Dimension
+        LSTM(NOTE_MODEL_LAYER_1, return_sequences=True),
+        LSTM(NOTE_MODEL_LAYER_2, return_sequences=True),
+        # Lambda Add Dimension
+        Reshape((NUM_NOTES, NUM_TIMESTEPS, NUM_FEATURES)),
+        # Lambda Remove Dimension
+        Dense(OUTPUT_LAYER),
+        Activation('sigmoid')
     ])
-
-    nadam = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
-    model.compile(loss='categorical_crossentropy', optimizer=nadam, metrics=['accuracy'])
-
-    model.fit(training_examples, training_labels)
 
 if __name__ == '__main__':
     main()
+
