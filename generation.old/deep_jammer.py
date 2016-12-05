48cce5cc96b17e80e4774b81d22b5ee6efd549d2
Justin Svegliato <justin.svegliato1@marist.edu>
2016-12-05 16:47:15 -0500

    Implemented more stuff that I forgot about - check the diff

--- generation.old/deep_jammer.py
+++ generation.old/deep_jammer.py
@@ -29,19 +29,17 @@ EPSILON = np.spacing(np.float32(1.0))
 
 ARE_CHECKPOINTS_ENABLED = True
 CHECKPOINT_DIRECTORY = 'checkpoints'
-CHECKPOINT_THRESHOLD = 50
+CHECKPOINT_THRESHOLD = 2 
 
 ARE_ACCURACIES_SAVED = True
 ACCURACIES_DIRECTORY = 'accuracies'
-ACCURACIES_THRESHOLD = 50
-ACCURACY_DECAY_RATE = 0.9
+ACCURACIES_THRESHOLD = 2
 
 IS_GPU_USED = False 
 
 
 def train(model, pieces):
     loss_history = []
-    moving_average_loss = 0.0
 
     for epoch in xrange(EPOCHS):
         print 'Training on epoch %s/%s...' % (epoch, EPOCHS)
@@ -49,11 +47,9 @@ def train(model, pieces):
         X, y = piece_handler.get_piece_batch(pieces, BATCH_SIZE)
 
         loss, _ = model.train_on_batch(X, y)
-            
-        moving_average_loss = moving_average_loss * ACCURACY_DECAY_RATE + loss * (1 - ACCURACY_DECAY_RATE)
-        loss_history.append(moving_average_loss)
-
         print 'Loss =', loss
+
+        loss_history.append(loss)
 
         if ARE_CHECKPOINTS_ENABLED and epoch % CHECKPOINT_THRESHOLD == 0:
             filename = '%s/model-weights-%s.h5' % (CHECKPOINT_DIRECTORY, epoch)
@@ -64,8 +60,8 @@ def train(model, pieces):
                 filename = '%s/model-accuracies.txt' % ACCURACIES_DIRECTORY
 
                 f = open(filename, 'w')
-                for moving_average_loss in loss_history:
-                    f.write('%s\n' % moving_average_loss)
+                for new_loss in loss_history:
+                    f.write('%s\n' % new_loss)
             else:
                 import matplotlib.pyplot as plt
 
@@ -152,8 +148,6 @@ def get_training_model():
         Dropout(DROPOUT_PROBABILITY),
 
         TimeDistributed(Dense(OUTPUT_LAYER)),
-        Dropout(DROPOUT_PROBABILITY),
-
         Activation('sigmoid'),
 
         Lambda(reshape_1, output_shape=get_reshape_shape_1)
@@ -198,8 +192,6 @@ def get_composition_model():
         Dropout(DROPOUT_PROBABILITY),
 
         TimeDistributed(Dense(OUTPUT_LAYER)),
-        Dropout(DROPOUT_PROBABILITY),
-
         Activation('sigmoid'),
 
         Lambda(reshape_1, output_shape=get_reshape_shape_1)
@@ -211,7 +203,7 @@ def main():
     training_model = get_training_model()
 
     print 'Compiling the training model...'
-    # Note that these settings are based on the post (which uses lr=0.01 and eps=1e-6)
+    # Note that these settings are based on the blog post (which uses lr=0.01 and epsilon=1e-6)
     optimizer = Adadelta(lr=0.01, epsilon=1e-6)
     training_model.compile(loss=objective, optimizer=optimizer, metrics=['accuracy'])
 
